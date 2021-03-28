bbea58c9fab348d32296c050047c6807c020e832
Maverick6798 <64378437+Maverick6798@users.noreply.github.com>
2021-03-28 15:36:53 +0530

    Update gender_age.py

--- gender_age.py
+++ gender_age.py
@@ -1,15 +1,3 @@
-'''
-PyPower Projects
-Detect Gender and Age using Artificial Intelligence
-
-'''
-
-#Usage 
-# Step 1 : Go to command prompt and set working directory where the gender_age.py file is stored
-# Step 2 : Execute the following command to detect from image: python gender_age.py -i 1.jpg  
-# Step 3 : Execute the following command to detect from webcam: python gender_age.py
-
-
 # Import required modules
 import cv2 as cv
 import math
